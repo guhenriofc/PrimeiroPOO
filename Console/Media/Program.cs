5255986e370e48269158958e17d6d3bca7918a56
guhenriofc <you@example.com>
2020-11-12 16:49:06 -0200

    Algoritimo Media concluido

---
+++ Console/Media/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua primeira nota: ");
+            double nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua segunda nota: ");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+            string situacao = "";
+
+            Console.WriteLine("Media: " + media);
+
+            if(media > 7){
+                situacao = "Aprovado";
+            } else{
+                situacao = "Reprovado";
+            }
+
+            Console.WriteLine(situacao);
+        }
+    }
+}
