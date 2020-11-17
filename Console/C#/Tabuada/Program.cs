3e922e189658224e37836315234fd5d5ae039fd9
guhenriofc <you@example.com>
2020-11-17 15:48:20 -0200

    Tabuada simples

---
+++ Console/C#/Tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual tabuada deseja realizar? ");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int resultado;
+
+            for (int contador = 0; contador <= 10; contador++)
+            {
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+            }
+        }
+    }
+}
