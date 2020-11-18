0f25585e39a20f35c444fc9a4d61666cae9728d4
guhenriofc <you@example.com>
2020-11-18 16:42:26 -0200

    Invalidez de numero

---
+++ Console/C#/Numero Invalido/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Numero_Invalido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int nota;
+
+                do{
+                    
+                    Console.WriteLine("Digite uma nota entre 0 e 10: ");
+                    nota = int.Parse(Console.ReadLine());
+
+                    if(nota < 0 || nota > 10){
+                        Console.WriteLine("Número Inválido");
+                    }
+
+                } while(nota < 0 || nota > 10);
+
+                Console.WriteLine("Número Aceito");
+
+        }
+    }
+}
