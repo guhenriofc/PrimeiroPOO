fc6fa001d2c12bed2fe3af6f40566ab93f7a15c9
guhenriofc <you@example.com>
2020-11-25 16:57:18 -0200

    Ex. 4 finalizado

--- Console/C#/Ex. 4/Program.cs
+++ Console/C#/Ex. 4/Program.cs
@@ -6,7 +6,26 @@ namespace Ex._4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] valores = new double [10];
+            double maior = 0;
+            double menor = 1000000000000000;
+
+            Console.WriteLine("Digite dez valores; ");
+            for (var i = 0; i < 10; i++)
+            {
+                valores[i] = double.Parse(Console.ReadLine());
+                if(valores[1] > maior){
+                    maior = valores[i];
+                }
+                if(valores[i] < menor){
+                    menor = valores[i];
+                }
+
+                
+            }
+
+            Console.WriteLine($"O menor valor colocado foi {menor} e o maior valor foi {maior}");
+            
         }
     }
 }
