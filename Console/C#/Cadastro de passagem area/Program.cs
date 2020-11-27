356e6685082833e1bac7ae749927e083f2900727
guhenriofc <you@example.com>
2020-11-26 22:36:19 -0200

    Cadastro de passagem area

---
+++ Console/C#/Cadastro de passagem area/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cadastro_de_passagem_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
